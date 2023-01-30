f970acbabc43731cb27a4a50440e1d93c1156972
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-01-30 12:54:09 +0300

    Create routes for both the home and user components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import Home from './Home';
+import Home from './pages/Home';
+import Users from './pages/Users';
+import React from 'react';
+import { Route,Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-       <Home/>
+    <Routes>
+        <Route  path="/"  element={<Home/>}/>
+        <Route  path="/users"  element={<Users/>}/>
+    </Routes>
     </div>
   );
 }
