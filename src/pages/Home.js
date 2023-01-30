618361fc05da6e687089095a06f5fc5af11107e1
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-01-30 12:55:36 +0300

    Display a header displaying the name of the page

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import  './Home.css'
+
+export default class Home extends React.Component{
+    state = {
+        users:[],
+        albums:[],
+    }
+    
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ users }))
+      .catch(error => console.error(error));
+
+      fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(response => response.json())
+      .then(albums => this.setState({ albums }))
+      .catch(error => console.error(error));
+    }
+
+    render(){
+        return(
+                <ol  className="containerElement">
+                            {this.state.users.map(user => (
+                            <li key={user.id}>{user.name}</li>
+                            ))}
+                </ol>
+             
+        
+        )
+    }
+}
