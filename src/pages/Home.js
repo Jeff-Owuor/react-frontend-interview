6f7f503db16d19ddf3c242e3b5d9bbdf5eaf2c38
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-01-31 13:28:04 +0300

    Display user details to the page upon clicking user's name
    get the id from the url
    find the user targeted by their id
    Use the resulting object to access the values of the given user's  name,username,email,phone and website

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,18 @@
 import React from "react";
 import  '../Home.css'
 import  {Link}  from  'react-router-dom'
-import { Route,Routes } from 'react-router-dom';
-import  UserDetails  from "./UserDetails"
 
 class Home extends React.Component{
-    state = {
-        users:[],
-        albums:[],
-    }
-    
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ users }))
-      .catch(error => console.error(error));
-
-      fetch('https://jsonplaceholder.typicode.com/albums')
-      .then(response => response.json())
-      .then(albums => this.setState({ albums }))
-      .catch(error => console.error(error));
-    }
-
     render(){
         return(
             <div>
                 <ol  className="containerElement">
-                            {this.state.users.map(user => (
+                            {this.props.users.map(user => (
                            <li key={user.id}>
                            <Link to={`/user/${user.id}`}>{user.name}</Link>
                            </li>
                             ))}
                 </ol>
-               <Routes>
-                   <Route  path="user/:userId"  element={<UserDetails  users={this.state.users}/>}/>
-               </Routes>
              </div>
         
         )
---
+++ src/pages/User.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useParams } from 'react-router-dom';
+
+function UserDetails(props) {
+    let { id } = useParams();
+    let user = props.users.find(user => user.id === parseInt(id));
+
+    return (
+      <div>
+        <h2>{user.name}</h2>
+        <p>Username: {user.username}</p>
+        <p>Email: {user.email}</p>
+        <p>Phone:{user.phone}</p>
+        <p>Website{user.website}</p>
+      </div>
+    );
+  }
+
+export default UserDetails
+
+
