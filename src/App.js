053d8513c2c3ab89b113dfd30913479d9d2508d6
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-01-31 13:25:10 +0300

    move api call from home component to app component and pass the result as props to home component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import './App.css';
 import Home from './pages/Home';
 import React from 'react';
 import { Route,Routes } from 'react-router-dom';
+import UserDetails from './pages/User';
 
 
-function App() {
+class  App extends React.Component{
+        state = {
+          users:[],
+      }
+
+      componentDidMount(){
+          fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ users }))
+        .catch(error => console.error(error));
+      }
+  render(){
   return (
     <div className="App">
     <Routes>
-        <Route  path="/"  element={<Home/>}/>
-        
+        <Route  path="/home"  element={<Home  users={this.state.users}/>}/>
+        <Route  path="user/:id"  element={<UserDetails  users={this.state.users}/>}/>
     </Routes>
     </div>
   );
 }
-
+}
 export default App;
