6477ef5f9b3bbf1639ed9e198c8ecc08174cd112
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-02-02 12:57:32 +0300

    Create route for each album
    add a property of photos in the state and set it to an empty array
    define a fetch method to get the data of the photos url
    update the photos in the state with the result from the api
    pass the photos down as props to album component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React from 'react';
 import { Route,Routes } from 'react-router-dom';
 import UserDetails from './pages/User';
 import LandingPage from './pages/LandingPage'
+import Albums from './pages/Albums';
 
 
 class  App extends React.Component{
         state = {
           users:[],
           albums:[],
+          photos:[]
       }
 
       componentDidMount(){
@@ -22,14 +24,20 @@ class  App extends React.Component{
         .then(response => response.json())
         .then(albums => this.setState({ albums }))
         .catch(error => console.error(error));
+
+        fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(photos => this.setState({ photos }))
+        .catch(error => console.error(error));
       }
   render(){
   return (
     <div className="App">
     <Routes>
-        <Route  path="/home"  element={<Home  users={this.state.users}/>}/>
-        <Route  path="user/:id"  element={<UserDetails  users={this.state.users}  albums={this.state.albums}/>}/>
         <Route   path="/"  element={<LandingPage/>}></Route>
+        <Route  path="/home"  element={<Home  users={this.state.users} albums={this.state.albums}/>}/>
+        <Route  path="user/:id"  element={<UserDetails  users={this.state.users}  albums={this.state.albums}/>}/>
+        <Route  path="album/:id"  element={<Albums  photos={this.state.photos} albums={this.state.albums}/>}  />
     </Routes>
     </div>
   );
