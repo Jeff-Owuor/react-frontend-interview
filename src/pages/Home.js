7da5019189c02b5c733fa0e42fec1c7f7ced0520
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-02-02 10:37:26 +0300

    Use of bootstrap card to display user information

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,20 @@ import  {Link}  from  'react-router-dom'
 class Home extends React.Component{
     render(){
         return(
-            <div>
-                <ol  className="containerElement">
+            <div className="container">
+            <div className="row  mt-5">
                             {this.props.users.map(user => (
-                           <li key={user.id}>
-                           <Link to={`/user/${user.id}`}>{user.name}</Link>
-                           </li>
+                           <div className="card mt-2 mx-3" style={{width: "18rem"}} key={user.id}>
+                                <div className="card-body">
+                                    <h5 className="card-title"><Link to={`/user/${user.id}`} style={{color:"black",textDecoration:"none"}}>{user.name}'s  User  Card</Link></h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">Albums~</h6>
+                                    
+                                </div>
+                                </div>
+
                             ))}
-                </ol>
+                
+                </div>
              </div>
         
         )
@@ -20,3 +26,5 @@ class Home extends React.Component{
 }
 
 export default  Home
+
+//good background for this page : https://media.istockphoto.com/id/930427714/photo/abstract-geometric-background-in-pastel-trend-colors-with-notebook.jpg?s=612x612&w=0&k=20&c=oUqwqZ5deA5GMWWadY0BRKdlbUW1S3zXC_x52R2W8i8=
