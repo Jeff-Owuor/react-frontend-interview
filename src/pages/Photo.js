cdcda0094cc0bffa55e69a8d3178232995a0faf6
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-02-08 16:09:07 +0300

    Add image to the photo route
    add css link

--- src/pages/Photo.js
+++ src/pages/Photo.js
@@ -1,13 +1,16 @@
 import React  from 'react'
 import { useParams} from 'react-router-dom';
+import './Photo.css'
 
 function Photo(props){
       let { id } = useParams();
     let photo = props.photos.find(photo => photo.id === parseInt(id));
     return(
-        <div>
+        <div className='photo'>
+               <h1>{photo.title}</h1>
+                <img  src={photo.url}  className="image"/>
                 <input  value={photo.title}  onChange={(e)=>props.handleTitleChange(e,parseInt(id))}/>
-                <button  onClick={()=>props.updateUser(id)}>Update</button>
+                <button  onClick={()=>props.updateUser(id)}  className="button">Update</button>
         </div>
     )
 }
