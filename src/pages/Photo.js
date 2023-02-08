cd4a1394d1fe71a2348107646e4764b8341a040b
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-02-08 11:19:06 +0300

    pass the functions to handle change in the input field and make put request to the photo component

--- src/pages/Photo.js
+++ src/pages/Photo.js
@@ -4,25 +4,10 @@ import { useParams,Link } from 'react-router-dom';
 function Photo(props){
       let { id } = useParams();
     let photo = props.photos.find(photo => photo.id === parseInt(id));
-    const [photoUpdate,setPhotoUpdate] = useState(photo)
-    const updateUser = () => {
-        fetch(`https://jsonplaceholder.typicode.com/photos/${id}`,
-           {
-            method:'PUT',
-            headers:{
-                'Accept':'application/json',
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify(photoUpdate)
-           })
-           .then(response => response.json())
-           .then(photo => setPhotoUpdate({ photo }))
-           .catch(error => console.error(error));
-    }
     return(
         <div>
-            <form>
-                <input  value={photoUpdate.title}  onChange={(e)=>{setPhotoUpdate(e.target.value)}} name="title"/>
+            <form onSubmit={()=>props.updateUser(id)}>
+                <input  value={photo.title}  onChange={()=>props.handleTitleChange(parseInt(id))} name="title"/>
                 <button>Update</button>
             </form>
         </div>
