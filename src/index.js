70ed42c0a36746a85592347c92faf4ce24af6519
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-03-30 09:17:19 +0300

    declare the redux store to be used for the entire application
    import store from the redux folder
    import loadUserData, loadAlbumData, loadPhotoData
    
    dispatch the above imported actions

--- src/index.js
+++ src/index.js
@@ -4,13 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import  {  loadUserData } from './redux/actions/retrieveUserData'
+import  {  loadAlbumData } from './redux/actions/retrieveAlbumData'
+import  {  loadPhotoData } from './redux/actions/retrievePhotoData'
+
+store.dispatch(loadUserData)
+store.dispatch(loadAlbumData)
+store.dispatch(loadPhotoData)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter>
-         <App />
-    </BrowserRouter>    
+     <Provider  store={store}>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>    
+     </Provider>
   </React.StrictMode>
 );
 
