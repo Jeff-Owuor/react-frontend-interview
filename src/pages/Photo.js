37674d21cb38968e14835736166e570f65041c5e
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-03-30 09:23:02 +0300

    Use useSelector hook to get the value from the state
    Iterate through the above variable to display photos on the page

--- src/pages/Photo.js
+++ src/pages/Photo.js
@@ -1,23 +1,29 @@
 import React  from 'react'
 import { useParams} from 'react-router-dom';
 import './Photo.css'
+import { useSelector } from 'react-redux';
 import Navbar from "./Navbar";
 
-function Photo(props){
+function Photo(){
+    let photos = useSelector(elem=>elem.photos[0])
       let { id } = useParams();
-    let photo = props.photos.find(photo => photo.id === parseInt(id));
+    let photo = photos.find(photo => photo.id === parseInt(id));
+    if(photos){
     return(
         <div>
               <Navbar   style={{width:"100vw"}}/>
         <div className='photo'>
                <h1>{photo.title}</h1>
                 <img  src={photo.url}  className="image"/>
-                <input  value={photo.title}  onChange={(e)=>props.handleTitleChange(e,parseInt(id))}/>
-                <button  onClick={()=>props.updateUser(id)}  className="button">Update</button>
+                {/* <input  value={photo.title}  onChange={(e)=>props.handleTitleChange(e,parseInt(id))}/>
+                <button  onClick={()=>props.updateUser(id)}  className="button">Update</button> */}
         </div>
         </div>
     )
-
+}
+  return (
+    <h3>Loading ...</h3>
+  )
 }
 
 
