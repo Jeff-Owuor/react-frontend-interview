025f7f6720dd393c5b7347488269990120152c11
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-04-02 16:51:35 +0300

    Make data persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,17 @@ import {createStore,applyMiddleware} from "redux"
 import appReducers from "./reducers/appReducer"
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
-const store = createStore(appReducers, composedEnhancer)
 
-export default store
+const persistConfig = {
+    key: "root",
+    storage
+};
+const persistedReducer = persistReducer(persistConfig, appReducers);
+const store = createStore(persistedReducer, composedEnhancer)
+const persistor = persistStore(store);
+
+export default store;
